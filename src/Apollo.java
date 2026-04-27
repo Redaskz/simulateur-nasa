f83c3a68e0d01ce2a37bdd418cca768a5c2a5deb
rreda <reda.rahmani@ynov.com>
2026-04-27 14:19:20 +0200

    Modele de la capsule Apollo

---
+++ src/Apollo.java
@@ -0,0 +1,12 @@
+
+// Modele concret de la capsule Apollo.
+public class Apollo extends Capsule {
+    public Apollo() {
+        super("Apollo", true, 3, 5.6, 200);
+    }
+
+    @Override
+    public double calculerIndiceSecurite() {
+        return 8.7;
+    }
+}
