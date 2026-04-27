91ec24a4b419c74e18e4be6ac16264d404d13bd6
rreda <reda.rahmani@ynov.com>
2026-04-27 15:25:27 +0200

    Lanceur Ariane 5

---
+++ src/Ariane5.java
@@ -0,0 +1,11 @@
+// Modele concret du lanceur Ariane 5.
+public class Ariane5 extends Lanceur {
+    public Ariane5() {
+        super("Ariane 5", false, 2, 700, 20, 180, 80);
+    }
+
+    @Override
+    public double calculerPousseeMax() {
+        return 13300;
+    }
+}
