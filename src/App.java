d16dcd08077daca88dc4be67a9e3f3dbbf182222
rreda <reda.rahmani@ynov.com>
2026-04-27 15:23:48 +0200

    start programme et simulateur principal

---
+++ src/App.java
@@ -0,0 +1,7 @@
+// Lance le programme et cree le simulateur principal.
+public class App {
+    public static void main(String[] args) {
+        Simulateur simulateur = new Simulateur();
+        simulateur.demarrer();
+    }
+}
