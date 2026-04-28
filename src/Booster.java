6b52fff449caae4d909938aef21f884d7d4dcab1
rreda <reda.rahmani@ynov.com>
2026-04-28 13:40:31 +0200

    Booster pour augmenter la puissance de la fusee

---
+++ src/Booster.java
@@ -0,0 +1,35 @@
+// Represente un booster ajoute a la fusee pour augmenter ses capacites.
+public class Booster {
+    private String nom;
+    private double pousseeAdditionnelle;
+    private double masse;
+    private double prix;
+
+    public Booster(String nom, double pousseeAdditionnelle, double masse, double prix) {
+        this.nom = nom;
+        this.pousseeAdditionnelle = pousseeAdditionnelle;
+        this.masse = masse;
+        this.prix = prix;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getPousseeAdditionnelle() {
+        return pousseeAdditionnelle;
+    }
+
+    public double getMasse() {
+        return masse;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    @Override
+    public String toString() {
+        return nom + " | masse=" + masse + " t | prix=" + prix + " M€";
+    }
+}
