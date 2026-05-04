a47745859cc6190b7dd259840ebbc6163114da83
rreda <reda.rahmani@ynov.com>
2026-05-04 13:32:13 +0200

    falcon 9

---
+++ src/Falcon9.java
@@ -0,0 +1,11 @@
+// Modele concret du lanceur Falcon 9.
+public class Falcon9 extends Lanceur {
+    public Falcon9() {
+        super("Falcon 9", true, 0, 500, 22, 60, 55);
+    }
+
+    @Override
+    public double calculerPousseeMax() {
+        return 7607;
+    }
+}
